7f661b974a646033231783662649bed4cdd133a5
Aryaman Sriram <crazycheetah42@outlook.com>
2024-05-08 20:28:51 -0400

    Big visual commit

--- install.sh
+++ install.sh
@@ -32,6 +32,7 @@ chown -R $username:$username /home/$username
 nala install feh bspwm sxhkd kitty rofi polybar compton thunar lxpolkit x11-xserver-utils unzip wget pulseaudio pavucontrol -y
 # Installing Other less important Programs
 nala install neofetch flameshot psmisc virt-manager remmina pitivi lxappearance papirus-icon-theme fonts-noto-color-emoji lightdm -y
+sudo apt install autoconf gcc make pkg-config libpam0g-dev libcairo2-dev libfontconfig1-dev libxcb-composite0-dev libev-dev libx11-xcb-dev libxcb-xkb-dev libxcb-xinerama0-dev libxcb-randr0-dev libxcb-image0-dev libxcb-util0-dev libxcb-xrm-dev libxkbcommon-dev libxkbcommon-x11-dev libjpeg-dev -y
 
 # Download Nordic Theme
 cd /usr/share/themes/
@@ -69,6 +70,15 @@ nala install brave-browser -y
 # Enable graphical login and change target from CLI to GUI
 systemctl enable lightdm
 systemctl set-default graphical.target
+
+git clone https://github.com/Raymo111/i3lock-color.git
+cd i3lock-color
+./install-i3lock-color.sh
+cd ..
+rm -r i3lock-color
+wget https://raw.githubusercontent.com/betterlockscreen/betterlockscreen/main/install.sh -O - -q | sudo bash -s system
+mv background.jpg ~/Pictures/
+betterlockscreen -u "~/Pictures/background.jpg"
 
 # Polybar configuration
 bash scripts/changeinterface
